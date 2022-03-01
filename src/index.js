66ad0dd1ed9a6130065d1c66ef0f526d2d9a4e24
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-01 17:17:59 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import _ from 'lodash';
+import _ from 'lodash';//eslint-disable-line
 import './style.css';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
   return element;
 }
 
