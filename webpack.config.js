59eecdc6e764177f8014ec4e143ffd15811824c4
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-01 15:33:33 +0100

    Setup webpack and Install dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   devServer: {
     static: './dist',
   },
