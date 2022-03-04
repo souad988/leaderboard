c56df8b7c1950398cf21deaee497b769f0cf6e5b
souad988 <59707859+souad988@users.noreply.github.com>
2022-03-04 14:56:34 +0100

    Add requests success and error msgs

--- src/modules/addToDom.js
+++ src/modules/addToDom.js
@@ -1,28 +1,33 @@
+const addNewScore = async (game, user, score, request) => {
+  const response = await game.addScore(user, score, request);
+  return response;
+};
 
+const createScoreElement = (score) => `<li class="score_row">${score.user}:${score.score}</li>`;
 
-const addNewScore=(game,user,score,request)=>{
-     game.addScore(user,score,request).then(res=>{
-     if(res.status===201){
-      alert('score added successfuly.');
-    }else{
-      alert('server doesn\'t respond');
-    }
-  }) 
-}
-
-
-
-const createScoreElement=(score)=>{
-  return `<li class="score_row">${score.user}:${score.score}</li>`;
-}
-
-const addScoresToDom = async(request,game,ulElement)=>{
-  var scores=await game.getScores(request);
-  ulElement.innerHTML='';
-  scores.result.map(item=>{
-    ulElement.innerHTML+=createScoreElement(item);
+const addScoresToDom = async (request, game, ulElement) => {
+  const scores = await game.getScores(request);
+  ulElement.innerHTML = '';
+  scores.result.forEach((item) => {
+    ulElement.innerHTML += createScoreElement(item);
   });
-}
+};
 
-export {addNewScore,addScoresToDom};
-
+const toggleMsg = (msg, classname, toggle, p) => {
+  if (toggle === 'show') {
+    p.classList.add(classname);
+    p.innerHTML = msg;
+  } else {
+    p.classList.remove(classname);
+    p.innerHTML = '';
+  }
+};
+const displayMsg = async (msg, classname, p, delay) => new Promise((resolve) => {
+  toggleMsg(msg, classname, 'show', p);
+  setTimeout(() => {
+    resolve(toggleMsg('', classname, 'hide', p));
+  }, delay);
+});
+export {
+  addNewScore, addScoresToDom, toggleMsg, displayMsg,
+};
--- src/modules/games.js
+++ src/modules/games.js
@@ -1,16 +1,16 @@
-
 export default class Game {
   constructor(id) {
-    this.id =id; 
+    this.id = id;
     this.scores = [];
   }
 
-  addScore(user,score,request){
-    return request.post([this.id,'scores'].join('/'),{user,score}) 
-    }
-  async getScores(request){
-    var response=await request.get([this.id,'scores'].join('/'));
-    this.scores=response.json();
-    return this.scores;  
-}
+  addScore(user, score, request) {
+    return request.post([this.id, 'scores'].join('/'), { user, score });
+  }
+
+  async getScores(request) {
+    const response = await request.get([this.id, 'scores'].join('/'));
+    this.scores = response.json();
+    return this.scores;
+  }
 }
